086389d591355eb883e5f178f2a7c732f948e7dc
wararyo <wararyo@gmail.com>
2017-09-23 14:57:35 +0900

    とりあえずミニゲームを実行して勝敗を受け取る

---
+++ Assets/Scripts/Commander.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Commander {
+
+    public static MiniGame[] Minigames;
+
+    public static Player[] Players;
+    
+    static Commander()
+    {
+        Players = new Player[]{
+            new Player("いちたろう",0),
+            new Player("じろう",1),
+        };
+
+        Minigames = new MiniGame[]{
+            new MiniGame("Love Triangle(終了しない不具合あり Alt+F4で終了)","MiniGames/LoveTriangle/LoveTriangle.exe"),
+            new MiniGame("ネタざんまい","MiniGames/NetaZanmai/NetaZanmai.exe"),
+            new MiniGame("ESCAPE SPY","MiniGames/ESCAPESPY/ESCAPESPY.exe"),
+            new MiniGame("Beach Gun Dash","MiniGames/BeachGunDash/BeachGunDash.exe"),
+        };
+    }
+}
+
+public class MiniGame
+{
+    public string name;
+    public string path;
+
+    public MiniGame(string name,string path)
+    {
+        this.name = name;
+        this.path = path;
+    }
+}
+
+public class Player
+{
+    public string name;
+    public int characterId;
+    public int point = 0;
+
+    public Player(string name,int characterId)
+    {
+        this.name = name;
+        this.characterId = characterId;
+    }
+}
---
+++ Assets/Scripts/ShuffleManager.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class ShuffleManager : MonoBehaviour
+{
+
+    public Text nextMiniGameText;
+
+    int nextMiniGameID = 0;
+
+    [NonSerialized]
+    public int winner = 0;
+
+    bool isGameStartWaiting = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        nextMiniGameID = UnityEngine.Random.Range(0, Commander.Minigames.Length);
+
+        nextMiniGameText.text = "次のゲームは\n" + Commander.Minigames[nextMiniGameID].name + "\nAを押してスタート";
+
+        isGameStartWaiting = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGameStartWaiting && Input.GetButtonDown("Fire1"))
+        {
+            isGameStartWaiting = false;
+            StartCoroutine(coroutine());
+        }
+    }
+
+    IEnumerator coroutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        ExecuteMiniGame();
+    }
+
+    void ExecuteMiniGame()
+    {
+        string FilePath = getPath() + Commander.Minigames[nextMiniGameID].path;
+
+        System.Diagnostics.Process process = new System.Diagnostics.Process();
+        process.StartInfo.FileName = FilePath;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.UseShellExecute = false;
+        //process.OutputDataReceived += new System.Diagnostics.DataReceivedEventHandler(OutputHandler);
+        process.StartInfo.CreateNoWindow = false;
+        process.EnableRaisingEvents = true;
+        process.Exited += new System.EventHandler(Process_Exit);
+        process.StartInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(FilePath);
+        process.StartInfo.Arguments =
+            Commander.Players[0].name + " " +
+            Commander.Players[0].characterId + " " +
+            Commander.Players[1].name + " " +
+            Commander.Players[1].characterId;
+
+        process.Start();
+        //process.BeginOutputReadLine();
+        process.WaitForExit();
+        string stdout = process.StandardOutput.ReadToEnd();
+        if (!string.IsNullOrEmpty(stdout))
+        {
+            Debug.Log(stdout);
+            if (stdout.StartsWith("1"))//1Pの勝ち
+            {
+                nextMiniGameText.text = "1Pの勝ち！";
+                winner = 1;
+            }
+            else if (stdout.StartsWith("2"))//2Pの勝ち
+            {
+                nextMiniGameText.text = "2Pの勝ち！";
+                winner = 2;
+            }
+        }
+        if (winner == 0) nextMiniGameText.text = "勝敗情報が送信されずに終了しました";
+        StartCoroutine(GotoNextGame());
+    }
+
+    private void OutputHandler(object sender, System.Diagnostics.DataReceivedEventArgs args)
+    {
+        if (!string.IsNullOrEmpty(args.Data))
+        {
+            Debug.Log(args.Data);
+            if (args.Data == "1")//1Pの勝ち
+            {
+                nextMiniGameText.text = "1Pの勝ち！";
+                winner = 1;
+            }
+            else if (args.Data == "2")//2Pの勝ち
+            {
+                nextMiniGameText.text = "2Pの勝ち！";
+                winner = 2;
+            }
+        }
+    }
+
+    // プロセス終了時.
+    private void Process_Exit(object sender, System.EventArgs e)
+    {
+        System.Diagnostics.Process proc = (System.Diagnostics.Process)sender;
+
+        // プロセスを閉じる.
+        proc.Kill();
+    }
+
+    private IEnumerator GotoNextGame()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadSceneAsync("Shuffle");
+    }
+
+    public string getPath()
+    {
+        string result = Application.dataPath;
+        if (Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            // MacOSでビルドした場合のパス(.appファイルの同階層のパス)
+            result = result + "/../../";
+        }
+        else if (Application.platform == RuntimePlatform.WindowsPlayer)
+        {
+            // Windowsでビルドした場合のパス(.exeファイルの同階層のパス)
+            result = result + "/../";
+        }
+        else
+        {
+            // それ以外の場合(Assets/Resourcesフォルダのパス)
+            result = result + "/../";
+        }
+        return result;
+    }
+}
