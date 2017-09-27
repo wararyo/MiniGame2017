a3258aa93d9e53ec559b64eea0731156b76621da
wararyo <wararyo@gmail.com>
2017-09-27 17:46:58 +0900

    UIインポート　ちょっと実装

---
+++ Assets/MiniGameCard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGameCard : MonoBehaviour {
+
+	public Image thumbImage;
+	public Text nameText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Initialize(Texture thumb,string name){
+		thumbImage.material.mainTexture = thumb;
+		nameText.text = name;
+	}
+}
